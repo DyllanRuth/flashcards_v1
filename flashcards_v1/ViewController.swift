92de31635b16d272d0169bd22c3dce6cd6a50209
Dyllanruth <Dyllan.ruth@gmail.com>
2022-03-31 15:38:51 -0500

    First iteration, Please view Flashcards V2

--- flashcards_v1/ViewController.swift
+++ flashcards_v1/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var AnswerBtn: UILabel!
+    @IBOutlet weak var Questionv1: UILabel!
+    var questions: [String] = []
+    var answers: [String] = []
+    var cardIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        buildCards()
+        Questionv1.text = questions[cardIndex]
+        AnswerBtn.text = "???"
     }
 
-
+    func buildCards() {
+        questions.append("THANK YOU For testing my app")
+        answers.append("follow me on LinkedIn")
+        questions.append("Which city is the capital of Japan?")
+        answers.append("Tokyo")
+        questions.append("2 to the power of 10 = ?")
+        answers.append("1024")
+        questions.append("When was the PVAMU establishment")
+        answers.append("1876")
+    }
+    
+    @IBAction func NextQ(_ sender: Any) {
+        cardIndex += 1
+        if cardIndex == questions.count {
+            cardIndex = 0
+        }
+        Questionv1.text = questions[cardIndex]
+        AnswerBtn.text = "???"
+    }
+    
+    @IBAction func ShowA(_ sender: Any) {
+        AnswerBtn.text = answers[cardIndex]
+    }
 }
 
